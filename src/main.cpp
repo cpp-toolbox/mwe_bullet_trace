482f130ba23a6e4171d048c082c09caeaf329efe
cuppajoeman <ccn@cuppajoeman.com>
2024-11-01 04:41:53 -0400

    feat: add working bullet trace

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,66 @@
 #include "window/window.hpp"
 #include "shader_cache/shader_cache.hpp"
 #include "batcher/generated/batcher.hpp"
+#include "vertex_geometry/vertex_geometry.hpp"
+#include "draw_info/draw_info.hpp"
 
 #include <cstdio>
 #include <cstdlib>
+#include <algorithm>
+#include <cmath>
+
+/*
+ * A bullet trace is the trace of a bullet fired from a gun
+ * when it exits the muzzle it is infinitelesly small, over time
+ * it scales up until it reaches a max height, then it continues
+ * moving at the same rate.
+ */
+class BulletTrace {
+  public:
+    glm::vec3 start_pos;
+    glm::vec3 travel_dir;
+    float time_since_fire_sec = 0;
+    float time_since_full_scale_sec = 0;
+    float max_height_of_trace = 1.0 / 25;
+    float travel_speed = 1;
+    // x / y which can be thought of as a function where you pass in
+    // the height and it yields the width to maintain the ratio
+    float aspect_ratio_of_trace_texture = 512.0 / 32.0;
+
+    BulletTrace(glm::vec3 start_pos, glm::vec3 travel_dir) : start_pos(start_pos), travel_dir(travel_dir) {}
+
+    IndexedVertexPositions get_trace_rect(double delta_time_sec) {
+        time_since_fire_sec += delta_time_sec;
+        float height = time_since_fire_sec * travel_speed;
+        float offset = 0;
+
+        if (height >= max_height_of_trace) {
+            height = max_height_of_trace;
+            time_since_full_scale_sec += delta_time_sec;
+            // we need to continue moving at the same rate that the width was moving
+            // as we updated the height, if the aspect ratio of x:y is 5:1, then
+            // when y changes by c, then x changes by 5 * c thus we must multiply
+            // by the ratio here to maintain the same speed
+            offset += aspect_ratio_of_trace_texture * time_since_full_scale_sec * travel_speed;
+        }
+
+        // note that width is really length, but we think in terms
+        // of an axis aligned setup to simplify the throught process
+        float width = aspect_ratio_of_trace_texture * height;
+
+        float center_of_rect = width / 2 + offset;
+
+        /*std::cout << center_of_rect << ", " << height << ", " << width << std::endl;*/
+
+        return {generate_rectangle_indices(),
+                generate_rectangle_vertices(start_pos.x + center_of_rect, start_pos.y + 0, width, height)};
+
+        // vertex geometry create a rect at (center_of_rect, 0, width, height)
+        // we do the vertical billboarded approach by considering it at as 2d problem
+        // finding the connecting vector and computing the transform to make it perpendicular
+        // but before doing any of that, just make sure that the above works properly
+    }
+};
 
 unsigned int SCREEN_WIDTH = 640;
 unsigned int SCREEN_HEIGHT = 480;
@@ -51,7 +108,8 @@ int main() {
     std::vector<spdlog::sink_ptr> sinks = {console_sink, file_sink};
 
     LiveInputState live_input_state;
-    GLFWwindow *window = initialize_glfw_glad_and_return_window(&SCREEN_WIDTH, &SCREEN_HEIGHT, "glfw window", false,
+    bool fullscreen = true;
+    GLFWwindow *window = initialize_glfw_glad_and_return_window(&SCREEN_WIDTH, &SCREEN_HEIGHT, "glfw window", true,
                                                                 false, false, &live_input_state);
 
     std::vector<ShaderType> requested_shaders = {ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR,
@@ -61,10 +119,20 @@ int main() {
 
     int width, height;
 
-    glm::vec4 color = glm::vec4(.5, .5, .5, 1);
+    glm::vec4 color = glm::vec4(1, 1, .25, 1);
     shader_cache.set_uniform(ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR, ShaderUniformVariable::RGBA_COLOR, color);
 
+    BulletTrace bullet_trace(glm::vec3(-0.5, 0, 0), glm::vec3(0, 1, 0));
+
+    double elapsed_time_sec = 0;
+    double time_since_last_fire_sec = 0;
+    double previous_time = glfwGetTime();
+
     while (!glfwWindowShouldClose(window)) {
+
+        double current_time = glfwGetTime();
+        double delta_time = current_time - previous_time;
+        previous_time = current_time;
 
         glfwGetFramebufferSize(window, &width, &height);
 
@@ -72,9 +140,21 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(indices, vertices_left);
-        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(indices, vertices_right);
+        auto ivp = bullet_trace.get_trace_rect(delta_time);
+
+        if (time_since_last_fire_sec > 0.25) {
+            bullet_trace.start_pos = glm::vec3(-0.75, std::sin(elapsed_time_sec), 0);
+            ;
+            bullet_trace.travel_speed = 1.2 + std::sin(elapsed_time_sec);
+            bullet_trace.time_since_fire_sec = 0;
+            bullet_trace.time_since_full_scale_sec = 0;
+            time_since_last_fire_sec = 0;
+        }
+
+        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(ivp.indices, ivp.xyz_positions);
         batcher.absolute_position_with_solid_color_shader_batcher.draw_everything();
+        time_since_last_fire_sec += delta_time;
+        elapsed_time_sec += delta_time;
 
         glfwSwapBuffers(window);
         glfwPollEvents();
